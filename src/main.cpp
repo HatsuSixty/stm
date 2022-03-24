8a89de41a95cc51d9187b670f9556420b64d8545
robertohermenegildodias <roberto011herminegildo@gmail.com>
2022-03-23 22:43:08 -0300

    Implement an save function for the bytecode

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,75 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <cassert>
+#include <fstream>
 
 #include "./operators.hpp"
 #include "./compiler.hpp"
+
+void save(std::string path, std::vector <Op> program)
+{
+    static_assert(OP_COUNT == 11 /* Exhaustive handling of OPs in save() */);
+    
+    std::fstream stream;
+    stream.open(path, std::ios::out);
+    
+    for (size_t i = 0; i < program.size(); ++i)
+    {
+	switch (program[i].type)
+	{
+	case OP_PRINT: {
+	    stream << 1;
+	} break;
+	
+	case OP_PLUS: {
+	    stream << 2;
+	} break;
+	
+	case OP_MINUS: {
+	    stream << 3;
+	} break;
+	
+	case OP_MULT: {
+	    stream << 4;
+	} break;
+	
+	case OP_DIV: {
+	    stream << 5;
+	} break;
+	
+	case OP_PUSH_INT: {
+	    stream << 6;
+	} break;
+	
+	case OP_DROP: {
+	    stream << 7;
+	} break;
+	
+	case OP_DUP: {
+	    stream << 8;
+	} break;
+	
+	case OP_SWAP: {
+	    stream << 9;
+	} break;
+	
+	case OP_ROT: {
+	    stream << 10;
+	} break;
+	
+	case OP_DEBUG_STACK: {
+	    stream << 11;
+	} break;
+	
+	default:
+	    std::cerr << "Unreachable\n";
+	    exit(2);
+	}
+    }
+
+    stream.close();
+}
 
 std::vector <Op> program = {
     {.type = OP_PUSH_INT, .content = 34},
@@ -29,6 +96,7 @@ std::vector <Op> program = {
 
 int main()
 {
+    save("test.stasm", program);
     simulate_program(program);
     return 0;
 }
