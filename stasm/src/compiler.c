f720abd06a47f4c2ef17f681c291c4ce182afe00
robertohermenegildodias <roberto011herminegildo@gmail.com>
2022-04-12 14:44:14 -0300

    [STASM] The compiler is now capable of parsing lines

--- stasm/src/compiler.c
+++ stasm/src/compiler.c
@@ -2,7 +2,36 @@
 
 void compile_program(const char* fpath)
 {
-    (void) fpath;
-    fprintf(stderr, "TODO: Function `compile_program` is not implemented yet\n");
-    exit(1);
+    FILE* file = fopen(fpath, "rb");
+    char* buffer = 0;
+    size_t length;
+
+    if (file == NULL)
+    {
+        fprintf(stderr, "ERROR: Could not open `%s`: %s\n",
+                fpath, strerror(errno));
+        exit(1);
+    }
+
+    fseek(file, 0, SEEK_END);
+    length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    buffer = malloc(length);
+
+    if (buffer == NULL)
+    {
+        fprintf(stderr, "ERROR: Could not allocate memory for file: %s",
+                strerror(errno));
+        exit(1);
+    }
+    fread(buffer, 1, length, file);
+    fclose(file);
+    String_View source = sv_from_cstr(buffer);
+
+    while (source.count > 0) {
+        String_View line = sv_trim(sv_chop_by_delim(&source, '\n'));
+        if (line.count > 0) {
+            printf(""SV_Fmt"\n", (int) line.count, line.data);
+        }
+    }
 }
--- stasm/src/compiler.h
+++ stasm/src/compiler.h
@@ -3,6 +3,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stddef.h>
 
 #include "./sv.h"
 
